68122356ea2b3e8717b2e364e47725150a57cdf2
StrobeliteS <eminty.me@gmail.com>
2023-08-04 15:15:02 +1200

    edited .env and ignore files

---
+++ prisma/migrations/20230804031019_/migration.sql
@@ -0,0 +1,37 @@
+-- CreateTable
+CREATE TABLE "Cat" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "age" INTEGER,
+    "health" TEXT NOT NULL,
+    "isAdopted" BOOLEAN NOT NULL,
+
+    CONSTRAINT "Cat_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Customer" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+    "likes" TEXT,
+    "dislikes" TEXT,
+
+    CONSTRAINT "Customer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Car" (
+    "id" SERIAL NOT NULL,
+    "model" TEXT NOT NULL,
+    "make" TEXT NOT NULL,
+    "year" INTEGER NOT NULL,
+    "registration" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "color" TEXT NOT NULL,
+
+    CONSTRAINT "Car_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Car_registration_key" ON "Car"("registration");
