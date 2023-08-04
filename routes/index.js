9213b8ca0c95ee1cdbe47a07e8594f3164af21d1
StrobeliteS <eminty.me@gmail.com>
2023-08-04 14:02:01 +1200

    created a database using Prisma (an ORM)

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 // Import the Express module
 import express from 'express'
 
+// This should be declared under import indexRoutes from "./routes/index.js";
+import institutionRoutes from "./routes/institution.js"
+
 // Import the index controllers module
 import { get } from '../controllers/index.js'
 
@@ -9,6 +12,15 @@ const router = express.Router()
 
 // Create a GET route
 router.get('/', get)
+
+// This should be declared under const app = express();
+app.use(urlencoded({ extended: false })); // To parse the incoming requests with urlencoded payloads. For example, form data
+
+// This should be declared under app.use(urlencoded({ extended: false }));
+app.use(json()); // To parse the incoming requests with JSON payloads. For example, REST API requests
+
+// This should be declared under app.use("/", indexRoutes);
+app.use("/api/institutions", institutionRoutes);
 
 // Export the router
 export default router
---
+++ routes/institution.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+import {
+  createInstitution,
+  getInstitutions,
+  getInstitution,
+  updateInstitution,
+  deleteInstitution,
+} from "../controllers/institution.js";
+
+const router = express.Router();
+
+router.post("/", createInstitution);
+router.get("/", getInstitutions);
+router.get("/:id", getInstitution); //:id = route parameter. Retrieves the id from the request (req) URL. If URL is http://localhost:3000/api/institutions/1, :id will be 1
+router.put("/:id", updateInstitution);
+router.delete("/:id", deleteInstitution);
+
+export default router;
