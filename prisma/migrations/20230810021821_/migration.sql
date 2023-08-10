1209d1f4a66b0b9942514b6aa8df6e7d78bb3c5e
StrobeliteS <eminty.me@gmail.com>
2023-08-10 14:19:25 +1200

    migrated stuff

--- prisma/migrations/20230810021821_/migration.sql
+++ prisma/migrations/20230810021821_/migration.sql
@@ -1,4 +1,16 @@
 -- CreateTable
+CREATE TABLE "Institution" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "region" TEXT NOT NULL,
+    "country" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Institution_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Cat" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
