d6e4bdf18a0e2f1be948a751e6c7822a76eb78e8
StrobeliteS <eminty.me@gmail.com>
2023-09-05 16:22:09 +1200

    Added new rows to Event model, tested in postman, added more validation

--- middleware/validation.js
+++ middleware/validation.js
@@ -67,8 +67,7 @@ const validatePostAnimal = (req, res, next) => {
       "string.base": "Rank must be a string",
       "string.empty": "Rank must not be empty",
       "any.required": "Rank is required",
-    }),
-    // ownerId: Joi.int().
+    })
   });
 
   const { error } = animalSchema.validate(req.body);
@@ -100,7 +99,27 @@ const validatePostParticipant = (req, res, next) => {
 
 const validatePostEvent = (req, res, next) => {
   const eventSchema = Joi.object({
-    //insert checks
+    eventName: Joi.string().min(5).max(50).required().messages({
+      "string.base": "Event Name should be a string",
+      "string.empty": "Event Name cannot be empty",
+      "string.min": "Event Name should have a minimum length of {#limit}",
+      "string.max": "Event Name should have a maximum length of {#limit}",
+      "any.required": "Event Name is required",
+    }),
+    location: Joi.string().min(5).max(50).required().messages({
+      "string.base": "Location should be a string",
+      "string.empty": "Location cannot be empty",
+      "string.min": "Location should have a minimum length of {#limit}",
+      "string.max": "Location should have a maximum length of {#limit}",
+      "any.required": "Location is required",
+    }),
+    date: Joi.date().greater('1-1-1990').max('12-12-2025').required().messages({
+      "date.base": "Date should not be empty",
+      "date.empty": "Date should not be empty",
+      "date.greater": "Date must be greater than {#limit}",
+      "date.max": "Date must be less than {#limit}",
+      "any.required": "Date is required"
+    })
   });
 
   const { error } = eventSchema.validate(req.body);
@@ -132,14 +151,34 @@ const validatePostAward = (req, res, next) => {
 
 const validatePostColosseum = (req, res, next) => {
   const colosseumSchema = Joi.object({
-     colosseumName: Joi.string().min(5).max(20).required().messages({
+     colosseumName: Joi.string().min(5).max(50).required().messages({
         "string.base": "Name should be a string",
         "string.empty": "Name cannot be empty",
         "string.min": "Name should have a minimum length of {#limit}",
         "string.max": "Name should have a maximum length of {#limit}",
         "any.required": "Name is required",
+     }),
+     country: Joi.string().min(5).max(50).required().messages({
+        "string.base": "Country should be a string",
+        "string.empty": "Country cannot be empty",
+        "string.min": "Country should have a minimum length of {#limit}",
+        "string.max": "Country should have a maximum length of {#limit}",
+        "any.required": "Country is required",
+     }),
+     city: Joi.string().min(3).max(50).required().messages({
+        "string.base": "City should be a string",
+        "string.empty": "City cannot be empty",
+        "string.min": "City should have a minimum length of {#limit}",
+        "string.max": "City should have a maximum length of {#limit}",
+        "any.required": "City is required",
+     }),
+     terrainType: Joi.string().min(3).max(50).required().messages({
+      "string.base": "Terrain Type should be a string",
+      "string.empty": "Terrain Type cannot be empty",
+      "string.min": "Terrain Type should have a minimum length of {#limit}",
+      "string.max": "Terrain Type should have a maximum length of {#limit}",
+      "any.required": "Terrain Type is required",
      })
-     
   });
 
   const { error } = colosseumSchema.validate(req.body);
@@ -152,38 +191,6 @@ const validatePostColosseum = (req, res, next) => {
 
   next();
 };
-
-// const validatePostCustomer = (req, res, next) => {
-//   const customerSchema = Joi.object({
-//     //insert checks
-//   });
-
-//   const { error } = customerSchema.validate(req.body);
-
-//   if (error) {
-//     return res.status(400).json({
-//       msg: error.details[0].message,
-//     });
-//   }
-
-//   next();
-// };
-
-// const validatePostTicket = (req, res, next) => {
-//   const ticketSchema = Joi.object({
-//     //insert checks
-//   });
-
-//   const { error } = ticketSchema.validate(req.body);
-
-//   if (error) {
-//     return res.status(400).json({
-//       msg: error.details[0].message,
-//     });
-//   }
-
-//   next();
-// };
 
 const validatePostTeam = (req, res, next) => {
   const teamSchema = Joi.object({
