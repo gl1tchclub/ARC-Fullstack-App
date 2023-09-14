5cd406511de012fc8b20352a05d17c3020340e8a
StrobeliteS <eminty.me@gmail.com>
2023-09-15 11:49:41 +1200

    Added seed data for some models

--- prisma/data/01-participantSeed.js
+++ prisma/data/01-participantSeed.js
@@ -1,9 +1,31 @@
 const name = "participant"
 const data = [
-  {
-    alias: "Joe Dane 5",
-    age: 16,
-  },
+  { alias:"Iver Ashpole", age:19 },
+  { alias:"Melina De Biasi", age:51 },
+  { alias:"Sansone Wandrich", age:79 },
+  { alias:"Cliff Stroud", age:38},
+  { alias:"Terrel Malkin", age:66},
+  { alias:"Jade Pigott", age:33},
+  { alias:"Taddeusz Oxer", age:45},
+  { alias:"Ginger Boyett", age:71},
+  { alias:"Liz Ambrosio", age:56},
+  { alias:"Ame Pavolini", age:40},
+  { alias:"Forester Cornwell", age:76},
+  { alias:"Patrizio Grazier", age:40},
+  { alias:"Kristan Gorringe", age:45},
+  { alias:"Aurthur Rushmare", age:22},
+  { alias:"Geno Lannon", age:68},
+  { alias:"Chad Blandford", age:26},
+  { alias:"Tommi Shankle", age:18},
+  { alias:"Coreen Romans", age:67},
+  { alias:"Emerson Rosevear", age:22},
+  { alias:"Carma Bowsher", age:69},
+  { alias:"Briny McPhillips", age:27},
+  { alias:"Reinaldo Crate", age:58},
+  { alias:"Maybelle Melding", age:48},
+  { alias:"Leia Kenewell", age:65},
+  { alias:"Ethelyn Keningham", age:71},
+  { alias:"Giacopo Rohan", age:32}
 ]
 
 export { name, data }
