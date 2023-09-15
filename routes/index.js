4fd4767bff4e90510f83dac77f8593a181c6dc7e
StrobeliteS <eminty.me@gmail.com>
2023-09-15 19:33:42 +1200

    Added index page comment

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
+/**
+ * @file Manages route related to index page
+ * @author Elizabeth Minty
+ */
 // Import the Express module
 import express from "express"
-
-// Import the index controllers module
-//import { get } from "../controllers/index.js"
 
 // Create an Express router
 const router = express.Router()
