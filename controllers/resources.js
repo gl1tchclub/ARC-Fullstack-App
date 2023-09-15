bc6691a9374067ea8ac0ac95aa75d098a12c25a5
StrobeliteS <eminty.me@gmail.com>
2023-09-15 12:55:43 +1200

    Added comments to resources

--- controllers/resources.js
+++ controllers/resources.js
@@ -5,13 +5,16 @@
 import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
+//Create objects of any given type (parameter)
 const create = (type) => {
   return async (req, res) => {
     try {
+      //Attempt to create a new object of given type with the data provided in the req body.
       await prisma[type].create({
         data: { ...req.body },
       })
 
+      //Retrieve all objects of the given type from the database after creation.
       const newType = await prisma[type].findMany()
 
       return res.status(201).json({
@@ -26,20 +29,22 @@ const create = (type) => {
   }
 }
 
-// Get All objects that match any given filters, sorted according to the user
+//Get All objects that match any given filters, sorted according to the user
 const getAll = async (req, res, type, include) => {
     try {
+      //Retrieve data from the specified type using Prisma.
       const typeModel = await prisma[type].findMany()
 
-      //checks if the given model exists
+      //Check if the given model exists; if not, return a 404 error.
       if (!typeModel) {
         return res.status(404).json({ msg: `Route for ${typeModel} does not exist`})
       }
       
-      //change orderBy and this const somehow -- use what already have?
+      //Extract query parameters like filters and orderBy.
       const { filters, orderBy } = req.query
       const match = {}
 
+      //If filters exist, parse and apply them to the 'match' object.
       if (filters) {
         const filterType = json.parse(filters)
 
@@ -50,7 +55,7 @@ const getAll = async (req, res, type, include) => {
         }
       }
 
-      //
+      //Define sorting parameters and a query object.
       const sortBy = req.query.sortBy || "name"
       const sortOrder = req.query.sortOrder === "desc" ? "desc" : "asc"
       const query = {
@@ -61,13 +66,16 @@ const getAll = async (req, res, type, include) => {
         skip: pageSize * (page - 1),
         take: !pageSize ? 25 : pageSize, //if pageSize not defined, default is 25
       }
+
+      //Retrieve objects from the typeModel based on the query.
       const objects = await typeModel.findMany(query)
 
-      //may not need this anymore
+      //If no objects are found, return a 404 error.
       if (typeModel.length === 0) {
         return res.status(404).json({ msg: `No ${type} found` })
       }
 
+      //Return a JSON response containing the retrieved data.
       return res.json({ data: objects })
     } catch (err) {
       return res.status(500).json({
@@ -76,18 +84,23 @@ const getAll = async (req, res, type, include) => {
     }
   }
 
+//Function getID that takes type (representing a model)
 const getID = (type) => {
   return async (req, res) => {
     try {
+      //Attempt to find a unique object of the given type based on the ID
       const typeId = await prisma[type].findUnique({
         where: { id: Number(req.params.id) },
       })
 
+      //Check if the object with the specified ID exists; if not, return a 404 error.
       if (!typeId) {
         res.status(404).json({
           msg: `${type} with ID ${res.params.id} does not exist`,
         })
       }
+
+      //Return a JSON response containing the retrieved data
       return res.json({ data: typeId })
     } catch (err) {
       return res.status(500).json({
@@ -97,24 +110,30 @@ const getID = (type) => {
   }
 }
 
+//Function update takes a given type and updates the object of that type by its ID
 const update = (type) => {
+  //Return an asynchronous request handler function.
   return async (req, res) => {
     try {
+      //Attempt to find the object of the given type based on the ID from the request.
       let updatedType = await prisma[type].findUnique({
         where: { id: Number(req.params.id) },
       })
 
+      //Check if the object with the specified ID exists; if not, return a 404 error.
       if (!updatedType) {
         return res.status(404).json({
           msg: `No ${type} with the id: ${req.params.id} found`,
         })
       }
-
+      
+      //Update the object with the new data from the request body
       updatedType = await prisma[type].update({
         where: { id: Number(req.params.id) },
         data: { ...req.body },
       })
 
+      //Return a JSON res displaying a success message along with the updated data.
       return res.json({
         msg: `${type} with the id: ${req.params.id} successfully updated`,
         data: updatedType,
@@ -127,23 +146,29 @@ const update = (type) => {
   }
 }
 
+//Function deleteType takes a given type and deletes the object of that type by its ID
 const deleteType = (type) => {
-  return async (req, res, type) => {
+  //Return an async request handler function.
+  return async (req, res) => {
     try {
+      //Attempt to find the object of the given type based on the ID.
       const deleteType = await prisma[type].findUnique({
         where: { id: Number(req.params.id) },
       })
 
+      //Check if the object with the specified ID exists; if not, return a 404 error.
       if (!deleteType) {
         return res.status(404).json({
           msg: `No ${type} with the id: ${req.params.id} found`,
         })
       }
 
+      //Delete the object from the database based on its ID.
       await prisma[type].delete({
         where: { id: Number(req.params.id) },
       })
 
+      // Return a JSON response indicating the successful deletion.
       return res.json({
         msg: `${type} with the id: ${req.params.id} successfully deleted`,
       })
