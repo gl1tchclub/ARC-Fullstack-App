028e2ca8a27e5a35fda72ee7546bd17efcc01fd0
mintep1-student <mintep1@student.op.ac.nz>
2023-09-30 00:29:37 +1300

    Made another test

--- test/test.js
+++ test/test.js
@@ -4,6 +4,8 @@
  */
 import chai from "chai";
 import chaiHttp from "chai-http";
+
+//assertion
 import { describe, it } from "mocha";
 import app from "../index.js";
 
@@ -14,84 +16,110 @@ const participant = {
     age: 19,
 };
 
-describe("Participants", () => {
-  it("should create participant", (done) => {
-    chai
-      .request(app)
-      .post("/api/participants")
-      .send(participant)
-      .end((req, res) => {
-        console.log(res)
+describe("ARC API - Participants", () => {
 
-        chai.expect(res.status).to.be.equal(201);
-        chai.expect(res.body).to.be.a("object");
-        chai
-          .expect(res.body.msg)
-          .to.be.equal("Participant successfully created");
+  //test GET route
+  describe("GET /api/participants", async() => {
+    it("It should GET all the participants", (done) => {
+      chai.request(app)
+        .get("/api/participants")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('array');
+          res.body.length.should.be.eq(1);
           done();
-      });
+        });
+    });
   });
+
+  //test GETID route
+
+
+  //test POST route
+
+
+  //test PUT route
+
+
+  //test DELETE route
+
+
+  // it("should create participant", async(done) => {
+  //     await chai.request(app)
+  //     .post("/api/participants")
+  //     .send(participant)
+  //     .end((res) => {
+  //       // console.log(res.body) //for testing
+
+  //       chai.expect(res.status).to.be.equal(201);
+  //       chai.expect(res.body).to.be.a("object");
+  //       chai
+  //         .expect(res.body.msg)
+  //         .to.be.equal("Participant successfully created");
+  //         done();
+  //     });
+  // });
 });
 
-it("should get all participants", (done) => {
-    chai
-      .request(app)
-      .get("/api/participants")
-      .end((req, res) => {
-        console.log(res) // This is useful for debugging
+// it("should get all participants", (done) => {
+//     chai
+//       .request(app)
+//       .get("/api/participants")
+//       .end((req, res) => {
+//         console.log(res) // This is useful for debugging
 
-        chai.expect(res.status).to.be.equal(200);
-        chai.expect(res.body).to.be.a("object");
-        chai.expect(res.body.data).to.be.a("array");
-        done();
-      });
-  });
+//         chai.expect(res.status).to.be.equal(200);
+//         chai.expect(res.body).to.be.a("object");
+//         chai.expect(res.body.data).to.be.a("array");
+//         done();
+//       });
+//   });
 
-  //more for filtering and pagination
+//   //more for filtering and pagination
 
-  it("should get participant by id", (done) => {
-    chai
-      .request(app)
-      .get("/api/participants/1")
-      .end((req, res) => {
-        console.log(res) // This is useful for debugging
+//   it("should get participant by id", (done) => {
+//     chai
+//       .request(app)
+//       .get("/api/participants/1")
+//       .end((req, res) => {
+//         console.log(res) // This is useful for debugging
 
-        chai.expect(res.status).to.be.equal(200);
-        chai.expect(res.body).to.be.a("object");
-        chai.expect(res.body.data).to.be.a("object");
-        done();
-      });
-  });
+//         chai.expect(res.status).to.be.equal(200);
+//         chai.expect(res.body).to.be.a("object");
+//         chai.expect(res.body.data).to.be.a("object");
+//         done();
+//       });
+//   });
 
-  it("should update participant by id", (done) => {
-    chai
-      .request(app)
-      .put("/api/participants/1")
-      .send(participant)
-      .end((req, res) => {
-        console.log(res) // This is useful for debugging
+//   it("should update participant by id", (done) => {
+//     chai
+//       .request(app)
+//       .put("/api/participants/1")
+//       .send(participant)
+//       .end((req, res) => {
+//         console.log(res) // This is useful for debugging
 
-        chai.expect(res.status).to.be.equal(200);
-        chai.expect(res.body).to.be.a("object");
-        chai
-          .expect(res.body.msg)
-          .to.be.equal("Participant with the id: 1 successfully updated");
-        done();
-      });
-  });
+//         chai.expect(res.status).to.be.equal(200);
+//         chai.expect(res.body).to.be.a("object");
+//         chai
+//           .expect(res.body.msg)
+//           .to.be.equal("Participant with the id: 1 successfully updated");
+//         done();
+//       });
+//   });
 
-  it("should delete participant by id", (done) => { 
-    chai
-      .request(app)
-      .delete("/api/participants/1")
-      .end((req, res) => {
-        console.log(res) // This is useful for debugging
+//   it("should delete participant by id", (done) => { 
+//     chai
+//       .request(app)
+//       .delete("/api/participants/1")
+//       .end((req, res) => {
+//         console.log(res) // This is useful for debugging
 
-        chai.expect(res.status).to.be.equal(200);
-        chai.expect(res.body).to.be.a("object");
-        chai
-          .expect(res.body.msg)
-          .to.be.equal("Participant with the id: 1 successfully deleted");
-        done();
-      });
-  });
+//         chai.expect(res.status).to.be.equal(200);
+//         chai.expect(res.body).to.be.a("object");
+//         chai
+//           .expect(res.body.msg)
+//           .to.be.equal("Participant with the id: 1 successfully deleted");
+//         done();
+//       });
+//   });
