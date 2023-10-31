2f76213f46f413c6848d07a9a7e01dd946359b32
mintep1-student <mintep1@student.op.ac.nz>
2023-10-31 13:12:30 +1300

    trying stuff

--- index.js
+++ index.js
@@ -40,6 +40,12 @@ app.use("/api/events", eventRoutes)
 app.use("/api/teams", teamRoutes)
 app.use("/api/colosseums", colosseumRoutes)
 app.use("/api/awards", awardRoutes)
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000/");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 app.use(cors({
   origin: "http://localhost:3000/"
 }))
