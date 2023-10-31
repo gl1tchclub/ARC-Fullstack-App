438d20e06c1971520b53876fb5e01a7a65e19241
mintep1-student <132317396+mintep1-student@users.noreply.github.com>
2023-11-01 10:41:06 +1300

    Update index.js

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ import awardRoutes from "./routes/award.js"
 const app = express()
 
 //Limit the number of API requests per minute to 100
-const limit = rateLimit({
-  windowMs: 60 * 1000,
-  max: 100,
-  message: async (req, res) => {
-    return "You have exceeded the number of requests per minute: 100. Please try again later"
-  },
-})
+//const limit = rateLimit({
+  //windowMs: 60 * 1000,
+  //max: 100,
+  //message: async (req, res) => {
+    //return "You have exceeded the number of requests per minute: 100. Please try again later"
+  //},
+//})
 
 // To parse the incoming requests with urlencoded payloads. For example, form data
 app.use(express.urlencoded({ extended: false }))
@@ -30,7 +30,7 @@ app.use(express.urlencoded({ extended: false }))
 // To parse the incoming requests with JSON payloads. For example, REST API requests
 app.use(express.json())
 app.use(cors())
-app.use(limit)
+//app.use(limit)
 
 // This code sets up middleware to route incoming requests to different parts of the application.
 // It directs requests to specific routes or endpoints for various functionalities.
